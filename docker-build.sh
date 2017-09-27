5dacad86409facf4fcd3e7f476a7232e8190e901
Justin Kolberg <amd.prophet@gmail.com>
2017-09-27 11:28:00 -0700

    install individual build tools on centos again

--- docker-build.sh
+++ docker-build.sh
@@ -7,8 +7,7 @@ install_dependencies() {
 	apt-get update
 	apt-get install -y build-essential curl fakeroot
     elif [ "$PLATFORM" = "centos" ]; then
-	yum -y install perl rpm-build yajl yajl-devel glibc-devel.i686
-	yum -y groupinstall 'Development Tools'
+	yum -y install perl rpm-build yajl yajl-devel make automake gcc gcc-c++ kernel-devel glibc-devel.i686
     fi
 }
 
