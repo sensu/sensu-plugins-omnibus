641b1d8ba3ab4410e3579b320ee17916052d7dab
Sean Porter <portertech@gmail.com>
2018-11-29 11:23:07 -0800

    add sensu-install to sensu-plugins-ruby
    Signed-off-by: Sean Porter <portertech@gmail.com>

---
+++ files/sensu-install/sensu-install.bat
@@ -0,0 +1,4 @@
+PATH=C:\opt\sensu-plugins-ruby\embedded\bin;%PATH%
+GEM_PATH=C:\opt\sensu-plugins-ruby\embedded\lib\ruby\gems\2.4.0;%GEM_PATH%
+
+C:\opt\sensu-plugins-ruby\embedded\bin\ruby C:\opt\sensu-plugins-ruby\embedded\bin\sensu-install %*
